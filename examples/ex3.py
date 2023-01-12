c1c1b1ecf68648532e174e47078757dc2f3a6bc8
Johannes Boog <johannesboog@yahoo.de>
2023-01-12 17:29:44 +0100

    [exmaples] update to new module structure

--- examples/ex3.py
+++ examples/ex3.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import netcdf2vtk.mapper as nvmp
+from netcdf2vtk.netcdf2vtk import Mapper
 
 # def variables ------------------------------------------------
-src_nc_path = "data/ex1_3.nc" # path of netcdf file
-ogs_vtu_path = "data/ogs.vtu" # path of ogs-mesh
-ogs_vtu_new_path = "ex3_new.vtu" # path of updated ogs-mesh
+nc_path = "data/ex1_3.nc" # path of netcdf file
+vtu_path = "data/ogs.vtu" # path of ogs-mesh
+vtu_new_path = "ex3_new.vtu" # path of updated ogs-mesh
 data_var_names = ["SWC_L01", "SWC_L02"] # names of the netcdf data variables
 map_func_type = 1 # def mapping func type 1: Voronoi, 2:Gaussian, 3:Shepard
-src_nc_crs = "EPSG:4326"   # coordinate system of netcdf file
-dst_crs = "EPSG:5684"   # coordinate sysetm of ogs-mesh file
+nc_crs = "EPSG:4326"   # coordinate system of netcdf file
+vtu_crs = "EPSG:5684"   # coordinate sysetm of ogs-mesh file
 nullvalue = -9999
 
 
 # run script -----------------------------------------------------
 # create mapper object
-mapper = nvmp.Mapper(src_nc_path,
-                    ogs_vtu_path,
+mapper = Mapper(nc_path,
+                    vtu_path,
                     data_var_names,
                     map_func_type,
-                    src_nc_crs,
-                    dst_crs,
+                    nc_crs,
+                    vtu_crs,
                     nullvalue)
 
-# extract data from src file
-mapper.get_scr_nc_data()
-
-# initialize a VTP object from coordinates in the netcdf file
-mapper.get_src_coords(lat_name = "lat",
+# set the names of coordinates variables in nc file
+mapper.set_nc_coord_names(lat_name = "lat",
                         lon_name = "lon",
                         time_name = "time")
-mapper.init_src_poly()
 
-# map netcdf data via the created VTP on the destination vtu
-mapper.map_data()
+# extract coordinate and variable data from nc file
+mapper.read_nc_coords()
+mapper.read_nc_data()
+
+# interpolate data from nc to vtu
+mapper.interpolate()
 
 # output the VTU with the mapped data
-mapper.write_out_vtu(path = ogs_vtu_new_path)
+mapper.write_out_vtu(path = vtu_new_path)
 
 
 # for convenience, all the above can be within two statements
-mapper2 = nvmp.Mapper(src_nc_path,
-                    ogs_vtu_path,
+mapper2 = Mapper(nc_path,
+                    vtu_path,
                     data_var_names,
                     map_func_type,
-                    src_nc_crs,
-                    dst_crs,
+                    nc_crs,
+                    vtu_crs,
                     nullvalue)
 
 mapper2.map(out_path = "ex3_mapper2_new_vtu.vtu",
